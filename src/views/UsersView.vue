8fb4642617fe0291ebfd7e63bb00b71a590c9383
Nesodev <ever.avendano@unmsm.edu.pe>
2025-05-14 00:02:49 -0500

    change url backend

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,19 +1,19 @@
 <template>
     <div id="users-view">
         <div class="head">
-            <svg @click="handleSideBar" width="25" height="26" viewBox="0 0 25 26" fill="none"
+            <svg :class="sidebarOpen ? 'selected' : 'no-selected'" @click="handleSideBar" width="25" height="25" viewBox="0 0 25 25" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
-                <rect y="0.5" width="25" height="25" rx="12.5" fill="#006CE0" />
-                <line x1="5.5" y1="8" x2="19.5" y2="8" stroke="white" stroke-width="2" />
-                <line x1="5.5" y1="13" x2="19.5" y2="13" stroke="white" stroke-width="2" />
-                <line x1="5.5" y1="18" x2="19.5" y2="18" stroke="white" stroke-width="2" />
+                <rect class="bg" width="25" height="25" rx="12.5" fill="white" />
+                <rect class="stroke" x="5.5" y="7" width="14" height="2" fill="#0F141A" />
+                <rect class="stroke" x="5.5" y="11.5" width="14" height="2" fill="#0F141A" />
+                <rect class="stroke" x="5.5" y="16" width="14" height="2" fill="#0F141A" />
             </svg>
         </div>
         <div class="body">
             <Transition name="sidebar-transition">
                 <div v-if="sidebarOpen" class="side-bar">
                     <div class="title">
-                        <h2>Gestión de usuarios</h2>
+                        <h2>Gestión de accesos</h2>
                         <svg @click="handleSideBar" width="8" height="25" viewBox="0 0 8 25" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <path d="M7 18.5L1.35355 12.8536C1.15829 12.6583 1.15829 12.3417 1.35355 12.1464L7 6.5"
@@ -22,22 +22,20 @@
                     </div>
                     <hr>
                     <nav>
-                        <li class="section">
-                            <div class="head-section">
-                                <svg width="13" height="10" viewBox="0 0 13 10" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M7.6464 9.14142C7.04712 9.85161 5.95288 9.85161 5.3536 9.14142L0.354758 3.21735C-0.468191 2.24209 0.225074 0.75 1.50116 0.75L11.4988 0.75C12.7749 0.75 13.4682 2.24209 12.6452 3.21735L7.6464 9.14142Z"
-                                        fill="#0F141A" />
-                                </svg>
-                                <h3>Usuarios y roles</h3>
-                            </div>
-                        </li>
+                        <router-link to="/products" class="sidebar-li" :class="{ 'active': $route.path === '/users' }">
+                            <h3>Usuarios</h3>
+                        </router-link>
+                        <router-link to="/materials" class="sidebar-li" :class="{ 'active': $route.path === '/users' }">
+                            <h3>Roles</h3>
+                        </router-link>
+                        <router-link to="/materials" class="sidebar-li" :class="{ 'active': $route.path === '/users' }">
+                            <h3>Permisos</h3>
+                        </router-link>
                     </nav>
                 </div>
             </Transition>
             <div class="content">
-                <h2>Contenido</h2>
+                <router-view></router-view>
             </div>
         </div>
     </div>
@@ -45,15 +43,16 @@
 
 <script setup>
 import { ref } from 'vue';
-const sidebarOpen = ref(false)
+const sidebarOpen = ref(true)
 const handleSideBar = () => {
     sidebarOpen.value = !sidebarOpen.value;
 }
 </script>
 
 <style scoped>
-
-h2, h3, p {
+h2,
+h3,
+p {
     padding: 0;
     margin: 0;
     font-family: sans-serif;
@@ -69,14 +68,24 @@ h2, h3, p {
 .head {
     width: 100%;
     height: 50px;
-    background: #EFEFEF;
     display: flex;
     align-items: center;
     justify-content: start;
-    padding: 0px 25px;
     box-sizing: border-box;
     border-bottom: 1px solid #B4B4BB;
 }
+
+.head .no-selected:hover .bg {
+    fill: #EBEBF0;
+}
+
+.head .selected .bg {
+    fill: #006CE0;
+}
+.head .selected .stroke {
+    fill: #fff;
+}
+
 
 .body {
     position: relative;
@@ -90,7 +99,6 @@ h2, h3, p {
 .content {
     width: 100%;
     height: 100%;
-    padding: 20px 30px;
     box-sizing: border-box;
 }
 
@@ -98,7 +106,6 @@ h2, h3, p {
     height: 100%;
     background: #fff;
     border-right: 1px solid #B4B4BB;
-    padding: 20px 25px;
     box-sizing: border-box;
 }
 
@@ -128,33 +135,30 @@ h2, h3, p {
     height: 100%;
     display: flex;
     flex-direction: column;
-    gap: 10;
+    gap: 10px;
 }
 
-nav .section {
-    width: 100%;
-    height: auto;
+nav .sidebar-li {
     list-style: none;
-}
-
-nav .section .head-section {
+    text-decoration: none;
     width: 100%;
     height: auto;
-    display: flex;
-    flex-direction: row;
-    gap: 8px;
-    align-items: center;
 }
 
-nav .section .head-section h3 {
-    font-size: 14px;
-    font-weight: bold;
+nav .sidebar-li h3 {
+    font-weight: 500;
+    color: #424650;
 }
 
 @media screen and (min-width: 769px) {
+    .head {
+        padding: 0px 15px;
+    }
+
     .side-bar {
         position: relative;
-        width: 250px;
+        width: 300px;
+        padding: 20px;
     }
 
     .sidebar-transition-enter-active,
@@ -164,20 +168,35 @@ nav .section .head-section h3 {
 
     .sidebar-transition-enter-from,
     .sidebar-transition-leave-to {
-        margin-left: -250px;
+        margin-left: -300px;
     }
 
     .sidebar-transition-enter-to,
     .sidebar-transition-leave-from {
         margin-left: 0px;
     }
+
+    .title h2 {
+        font-size: 18px;
+    }
+
+    nav .sidebar-li h3 {
+        font-size: 14px;
+    }
 }
 
 @media screen and (max-width: 768px) {
+    .head {
+        padding: 0px 20px;
+    }
+
     .side-bar {
         position: absolute;
-        width: 200px;
+        width: auto;
+        min-width: 200px;
+        max-width: 240px;
         z-index: 2;
+        padding: 15px 20px;
     }
 
     .sidebar-transition-enter-active,
@@ -193,6 +212,22 @@ nav .section .head-section h3 {
     .sidebar-transition-enter-to,
     .sidebar-transition-leave-from {
         left: 0px;
+    }
+
+    .title {
+        gap: 10px;
+    }
+
+    .title h2 {
+        font-size: 16px;
+    }
+
+    .side-bar hr {
+        margin-top: 15px;
+    }
+
+    nav .sidebar-li h3 {
+        font-size: 14px;
     }
 }
 </style>
